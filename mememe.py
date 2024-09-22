5b7fa27d1210ce041fe0d5423a6205186bd521ab
бомбяо <arskott2@gmail.com>
2024-09-22 13:09:27 +0300

    Add files via upload

---
+++ mememe.py
@@ -0,0 +1,15 @@
+#дз
+#файл 1, задача 1
+a = int(input('Введите число: '))
+for i in range(1, a+1):
+    print(i)
+
+#файл 1, задача 2
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+if a > b:
+    print(a)
+if b > a:
+    print(b)
+if a == b:
+    print('числа равны')
